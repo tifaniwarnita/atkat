3e125cb17ddba44d9178d609e45b1342af84a77a
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-02-10 09:32:58 +0700

    Solve main.js for Azwar

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 const electron = require('electron');
 // Module to control application life.
-const app = electron.app;
+const app = require('app');
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = require('browser-window');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
